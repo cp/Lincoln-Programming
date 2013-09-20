d0548d74f1c84851f4f9852c761475fe0f6d50e8
Colby Aley <colby@aley.me>
2013-09-19 18:39:20 -0700

    Adding basic form for signing up, and a text message when the user does so

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
-%w(sinatra active_record open-uri).each{|x| require x}
+%w(sinatra active_record open-uri twilio-ruby validates_phone_number).each{|x| require x}
 
-db = (URI.parse(ENV['DATABASE_URL']) || URI.parse('postgres://localhost/lincolnprogramming'))
+ENV['RACK_ENV'] == 'production' ? db = URI.parse(ENV['DATABASE_URL']) : db = URI.parse('postgres://localhost/lincolnprogramming')
+
+twilio = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_AUTH_TOKEN'])
 
 ActiveRecord::Base.establish_connection(
   :adapter  => 'postgresql',
@@ -12,8 +14,24 @@ ActiveRecord::Base.establish_connection(
 )
 
 class Member < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone, presence: true, uniqueness: true, phone_number: {:ten_digits => true}
 end
 
 get '/' do
   erb :index
+end
+
+post '/member/new' do
+  member = Member.new(name: params[:name], phone: params[:phone], email: params[:email])
+
+  if member.save!
+    twilio.account.messages.create(
+      :from => ENV['TWILIO_NUMBER'],
+      :to => '+1' + member.phone,
+      :body => "Welcome to Lincoln Programming, #{member.name}! We'll alert you here the morning of our next meeting."
+    )
+    redirect '/'
+  end
 end
