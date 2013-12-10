ab52a618f81a449367c32eddfd257e5e4aab832e
Colby Aley <colby@aley.me>
2013-12-09 16:15:37 -0800

    Adding some better docs, and forwarding stray messages on to admins. This allows us to know if someone has a question.

--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ class Member < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :phone, presence: true, uniqueness: true, phone_number: {:ten_digits => true}
 
+  # Mark the user as unsubscribed. Here's to never deleting data!
   def unsubscribe!
     self.update_attributes(unsubscribed: true)
   end
@@ -20,6 +21,7 @@ get '/' do
   erb :index
 end
 
+# This endpoint is reserved for the online form.
 post '/member/new' do
   member = Member.new(name: params[:name], phone: params[:phone], email: params[:email])
 
@@ -37,11 +39,12 @@ post '/member/new' do
   end
 end
 
+# This is the endpoint hit whenever someone messages our service.
 post '/message/new' do # Accept SMS messages through Twilio.
   status 400 unless validate_twilio_request
   body = params['Body']
   from = params['From'][2..-1]
-  
+
   # Since we don't yet have members' phone numbers,
   # this will allow them to text in their email, and
   # add their phone number to their account.
@@ -58,7 +61,7 @@ post '/message/new' do # Accept SMS messages through Twilio.
     end
   elsif body == 'unsubscribe'
     Member.find_by_phone(from).unsubscribe!
-    generate_sms_twiml "You have been unsubscribe, and you will not recieve any more notifications at this number."
+    generate_sms_twiml "You have been unsubscribed, and you will not recieve any more notifications at this number."
   elsif body.split[0] == 'blast'
     if member = Member.find_by_phone(from)
       if member.is_admin
@@ -69,11 +72,15 @@ post '/message/new' do # Accept SMS messages through Twilio.
       end
     end
   else
+    alert_admins(body, from)
     generate_sms_twiml "Hello! Welcome to the Lincoln Programming Club. Read more and sign up at lincolnprogramming.com"
   end
 
 end
 
+# This sends a message to all subscribed users.
+#
+# @param [String] message to send to users.
 def send_blast(message)
   # Sending them concurrently like a boss.
   hydra = Typhoeus::Hydra.new
@@ -88,12 +95,38 @@ def send_blast(message)
   hydra.run
 end
 
+# Simply forward the message on to admins
+# in case they're curious, or if someone
+# has something important to say.
+#
+# @param [String] message to send to admins.
+# @param [String] number incoming message was sent from.
+def alert_admins(message, number)
+  alert = "New message from #{number}: '#{message}'"
+
+  Member.where('phone IS NOT NULL').where(is_admin: true).each do |admin|
+    hydra.queue(Typhoeus::Request.new(
+      "https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_SID']}/Messages", 
+      method: :post,
+      body: { "To" => admin.phone, "From" => ENV['TWILIO_NUMBER'], "Body" => alert },
+      userpwd: "#{ENV['TWILIO_SID']}:#{ENV['TWILIO_AUTH_TOKEN']}"
+    ))
+  end
+  hydra.run
+end
+
+# Generates valid "TWIML" given some text.
+#
+# @param [String] text
 def generate_sms_twiml(text)
   Twilio::TwiML::Response.new do |r|
     r.Sms text
   end.text
 end
 
+# Ensures that the given request is valid. Only applies to production.
+#
+# @return [Boolean] whether or not the request is valid.
 def validate_twilio_request
   return true unless ENV['RACK_ENV'] == 'production'
   validator = Twilio::Util::RequestValidator.new(ENV['TWILIO_AUTH_TOKEN'])
